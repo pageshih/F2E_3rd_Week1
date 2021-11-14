93500c85a0d79585d4cb684a84b59c98cf0422ff
pageshih <a0981863141@gmail.com>
2021-11-15 00:16:28 +0800

    first update

---
+++ app/assets/js/all.js
@@ -0,0 +1,31 @@
+$(function() {
+  console.log('Hello Bootstrap5');
+});
+
+var swiper = new Swiper(".mySwiper", {
+  breakpoints: {
+    100:{
+      slidesPerView: 1.1,
+      spaceBetween: 10
+    },
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2.5,
+      spaceBetween: 10
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+    // when window width is >= 640px
+    980: {
+      slidesPerView: 4.5,
+      spaceBetween: 20
+    }
+  },
+  navigation: {
+    nextEl: ".button-next",
+    prevEl: ".button-prev",
+  },
+});
